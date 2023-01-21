80356de5ec2c42246812a46e51b6b29de8b89d72
546pvp <79662987+546pvp@users.noreply.github.com>
2023-01-21 12:54:28 +0100

    Added another method to combine strings

--- hello-world.js
+++ hello-world.js
@@ -2,5 +2,6 @@ var name = 'ted'
 var age = 5
 
 var message = 'my name is ' + name + ' and I am ' + age + ' years old'
-
-console.log(message)
+//you can use template literal (backticks). Example:
+var example = `my name is ${name} and i am ${age} years old` //as you can see it is faster
+console.log(message, example)
